43a578fcd5d27a0bb605999211f8903c9eb5967b
Binit kumar Shaw <binitkumarshaw063@gmail.com>
2025-07-03 05:40:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 11   //const value can't be change
+let accountName = "Binit"   // let value can be chane but there is no hoisting
+var city = "Durgapur" // used in previous version of js and can be hoisting
+state = "West Bengal" // this will also work but, not a good practice 
+
+console.log(accountID)
+console.table([accountID, accountName, city, state])
