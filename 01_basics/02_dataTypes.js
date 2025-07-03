7233af3ffbd3bf76a020b21215866912325e65a0
Binit kumar Shaw <binitkumarshaw063@gmail.com>
2025-07-03 05:44:36 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let name = "Binit" //String(can be in single quotes as well as in double quotes)
+let age  = 22 // Number, range - 2 - 2^53, bigint
+let isStudent = false // Boolean- true/ false
+
+console.log(typeof age); // Number
+// null - standalone value(Empty value)
+// undefined - no value assigned 
+// symbol - mostly used in react to find uniqueness
+
+// Non Primitive Data Type
+
+// object - 
+console.log(typeof name) // String
+console.log(typeof age) // Number
+console.log(typeof undefined) // Undefined
+console.log(typeof null) // object(it is a buf of JS)
+
+
